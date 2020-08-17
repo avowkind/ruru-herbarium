6452f2d8e7687072266cd84ca6725afe6d1e8db4
Andrew Watkins <andrew@groat.nz>
2020-08-18 08:33:04 +1200

    fixup config

---
+++ pages/api/get-config.js
@@ -0,0 +1,11 @@
+import nextConnect from 'next-connect';
+import middleware from '../../middlewares/middleware';
+import config from '../../lib/config'
+const handler = nextConnect();
+
+handler.use(middleware);
+handler.get(async (req, res) => {
+  res.send(config);
+});
+
+export default handler;
